3ccdcd15378798460bddb2b31bda309f3f01f26b
ryan <ryan@scorchthinks.dev>
2025-04-24 08:59:02 +0800

    getting to start

---
+++ README.md
@@ -0,0 +1,7 @@
+# Portfolio Website
+Lin Hengrui Ryan
+## Shorthands
+run before development
+```
+npx @tailwindcss/cli -i ./src/input.css -o ./src/main.css --watch
+```
--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
-import { gsap } from "gsap";
-    
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "./node_modules/gsap/index.js";
+
+document.addEventListener("DOMContentLoaded", start);
 
 // Initialize Lenis
 const lenis = new Lenis({
-    autoRaf: true,
-  });
-  
-  // Listen for the scroll event and log the event data
-  lenis.on('scroll', (e) => {
-    console.log(e);
-  });
+	autoRaf: true,
+});
 
-var start= gsap.timeline
+// Listen for the scroll event and log the event data
+lenis.on("scroll", (e) => {
+	console.log(e);
+});
+
+function start() {
+	console.log("domContentLoaded");
+	var startTimeline = gsap.timeline({ defaults: { duration: 1 } });
+	startTimeline.from("#header-text", { opacity: 0, y: 25, ease: "power1.in" });
+	startTimeline.play();
+}
